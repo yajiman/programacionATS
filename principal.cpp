1024efa035f456c58be95a0bebcddfc3c0451109
yajiman <36055803+yajiman@users.noreply.github.com>
2023-02-05 18:59:38 -0400

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -1,7 +1,7 @@
 #include "logica.h" //agrigo la coneccion a las funciones
 
 int main(){
-    menu();//conecto 
+    menu();//conecto con las funciones de logica
    cout<<"gracias por su tiempo";//agradesco el tiempo de las personas
     getchar();
 return 0;
