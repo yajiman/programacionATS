45ab4b2782e6c8e3bb9982c31b9f54c261efdd05
yajiman <yajalbel@hotmil.com>
2023-02-18 21:18:13 -0400

    probando los conflictos

--- principal.cpp
+++ principal.cpp
@@ -5,4 +5,4 @@ int main(){
    cout<<"gracias por su tiempo";//agradesco el tiempo de las personas
     getchar(); //no reconoce la pausa 
 return 0;
-}
+} // este es el fin de un programa que esta escrito en C++ de una forma completaS
